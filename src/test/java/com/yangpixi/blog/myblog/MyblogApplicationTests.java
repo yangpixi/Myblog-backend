80f85e5f17434fbeb38f130f3ba0a79f529fd098
yangpixi <yang20070135@gmail.com>
2025-09-03 21:42:57 +0800

    初始化项目

---
+++ src/test/java/com/yangpixi/blog/myblog/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangpixi.blog.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
